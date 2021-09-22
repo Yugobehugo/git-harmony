6fccd80350a733786aeaf7c9f142ba1f6938d8a7
Julien Cousin-Alliot <julien.cousinalliot@gmail.com>
2021-09-22 10:55:28 +0200

    Add julien's test (#16)

--- cypress/integration/julien.spec.js
+++ cypress/integration/julien.spec.js
@@ -5,9 +5,20 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
-describe('example to-do app', () => {
-    it('should visit a site', () => {
+describe('Test de Julien', () => {
+    it('Se connecter au site', () => {
         cy.visit('http://harmony.barbec.tk/')
     })
 
+    it('Homepage ?', () => {
+        cy.get('h1').should('have.text', "T'as reconnu l'équipe :")
+    })
+
+    it('Test form', () => {
+        cy.get('.new-member').type('Cypress')
+        cy.get('.new-occupation').type('Testing bot')
+        cy.get('.new-submit').click()
+        cy.get('li').last().should('have.text', "Cypress - Testing bot")
+
+    })
 })
